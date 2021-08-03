116137060e3b4bcd8b512b4c286e3c32da2b31f7
Tsuneki Umeda <umedat@amazon.co.jp>
2021-08-03 18:52:49 +0900

    refactor method name and main method logic for readability

--- src/ValidateISBN.java
+++ src/ValidateISBN.java
@@ -4,43 +4,33 @@ public class ValidateISBN {
     public static final int LONG_ISBN_LENGTH = 13;
     public static final int LONG_ISBN_MULTIPLIER = 10;
     public static final int SHORT_ISBN_MULTIPLIER = 11;
-    
+
     public boolean checkISBN(String isbn) {
         isbn = isbn.replaceAll("[^0-9X]", "");
         int isbnLength = isbn.length();
 
-        validateISBNLength(isbnLength);
-
         if (isbnLength == SHORT_ISBN_LENGTH) {
-            return isValid10DigitISBN(isbn);
-        } else {
-            return isValid13DigitISBN(isbn);
+            return isValidShortISBN(isbn);
+        } else if (isbnLength == LONG_ISBN_LENGTH) {
+            return isValidLongISBN(isbn);
         }
+        throw new NumberFormatException("ISBN numbers must be 10 or 13 digit long");
     }
 
-    private void validateISBNLength(int isbnLength) {
-        if (isbnLength != SHORT_ISBN_LENGTH && isbnLength != LONG_ISBN_LENGTH) {
-            throw new NumberFormatException("ISBN numbers must be 10 digit long");
-        }
-    }
-
-    private boolean isValid13DigitISBN(String isbn) {
-
+    private boolean isValidLongISBN(String isbn) {
         int total = 0;
-
         for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
             if (i % 2 == 0) {
                 total += Character.getNumericValue(isbn.charAt(i));
             } else {
-                total += Character.getNumericValue(isbn.charAt(i)) * 3;
+                total += 3 * Character.getNumericValue(isbn.charAt(i));
             }
         }
 
         return total % LONG_ISBN_MULTIPLIER == 0;
     }
 
-    private boolean isValid10DigitISBN(String isbn) {
-
+    private boolean isValidShortISBN(String isbn) {
         int total = 0;
         for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
             char isbnDigit = isbn.charAt(i);
@@ -54,7 +44,6 @@ public class ValidateISBN {
             } else {
                 total += Character.getNumericValue(isbnDigit) * (SHORT_ISBN_LENGTH - i);
             }
-
         }
 
         return total % SHORT_ISBN_MULTIPLIER == 0;
